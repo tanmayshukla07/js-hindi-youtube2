d534f8bf26ca6a1354cc01839756346e9221272c
tanmayshukla07 <shuklatushar784@gmail.com>
2025-10-05 21:21:08 +0000

    datatypes

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 144564
+let accountEmail = "tanmayakgec.ac.in"
+var accountPassword = "344433"
+accountCity =  "Indore"
+
+console.log([accountId,accountEmail,acco])
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,4 @@
+"use strict";//treat all js code as newer version
+let name = "tanmay"
+console.log(4+4)
+console.log(typeof name )
